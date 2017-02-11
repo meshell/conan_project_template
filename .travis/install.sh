a34d6124557d256db1660a8e162149ce597386d6
Michel Estermann <estermann.michel@gmail.com>
2017-02-12 00:54:16 +0100

    Added travis ci files.

---
+++ .travis/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+set -x
+
+if [[ "$(uname -s)" == 'Darwin' ]]; then
+    brew update || brew update
+    brew outdated pyenv || brew upgrade pyenv
+    brew install pyenv-virtualenv
+    brew install cmake || true
+
+    if which pyenv > /dev/null; then
+        eval "$(pyenv init -)"
+    fi
+
+    pyenv install 2.7.10
+    pyenv virtualenv 2.7.10 conan
+    pyenv rehash
+    pyenv activate conan
+else
+    pip install --user conan
+fi
+
+gem install bundler --no-document
+if [ "${COVERAGE}" == '1' ] ; then export PATH=$HOME/.local/bin:$PATH && pip install --user urllib3[secure] cpp-coveralls ; fi
---
+++ .travis/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+set -x
+
+if [[ "$(uname -s)" == 'Darwin' ]]; then
+    if which pyenv > /dev/null; then
+        eval "$(pyenv init -)"
+    fi
+    pyenv activate conan
+fi
+mkdir build && pushd build
+conan install .. --build missing
+cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} ..
+popd
+
+cmake --build build --target all --config ${BUILD_TYPE}
+cmake --build build --target run_feature_test --config ${BUILD_TYPE}
+cmake --build build --target Conan_cpp_project_template --config ${BUILD_TYPE}
+if [ "${COVERAGE}" == '1' ] ; then cmake --build build --target coverage --config ${BUILD_TYPE} ; fi
