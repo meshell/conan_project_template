4207bb76b5846a9cc03f458d4f9b329ee49c59d1
Michel Estermann <estermann.michel@gmail.com>
2017-03-26 17:46:09 +0200

    Install conan on osx

--- .travis/install.sh
+++ .travis/install.sh
@@ -8,7 +8,7 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     brew outdated pyenv || brew upgrade pyenv
     brew install pyenv-virtualenv
     brew install cmake || true
-
+    
     if which pyenv > /dev/null; then
         eval "$(pyenv init -)"
     fi
@@ -17,6 +17,7 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     pyenv virtualenv 2.7.10 conan
     pyenv rehash
     pyenv activate conan
+    pip install conan
 else
     pip install --user conan
 fi
