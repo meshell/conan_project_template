500572e53d36ade99eaa7febb7cb6728693fbec0
Michel Estermann <estermann.michel@gmail.com>
2017-03-27 21:16:54 +0200

    Use conan settings for travis build.

--- .travis/run.sh
+++ .travis/run.sh
@@ -10,11 +10,10 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     pyenv activate conan
 fi
 mkdir build && pushd build
-conan install .. --build missing
+conan install .. --build missing -s compiler=${compiler} -s compiler.version=${COMPILER_VERSION}
 cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} ..
 popd
 
 cmake --build build --target all --config ${BUILD_TYPE}
 cmake --build build --target run_feature_test --config ${BUILD_TYPE}
-cmake --build build --target Conan_cpp_project_template --config ${BUILD_TYPE}
 if [ "${COVERAGE}" == '1' ] ; then cmake --build build --target coverage --config ${BUILD_TYPE} ; fi
